2d03559bd0db9c376671ee0b4f933daf8bdcd304
David Lamparter <equinox@diac24.net>
2012-02-16 06:08:37 +0100

    zebra: revert pimd nexthop_active change
    this change seems bogus to me; kernel routes are routes added by other
    routing daemons. the intention seems to be to make recursive nexthops
    on kernel routes work, but either this needs to apply to static routes
    as well or it's simply wrong.
    
    either way this needs to be inspected outside of the pimd merge.
    
     * zebra/zebra_rib.c: reverted

--- zebra/zebra_rib.c
+++ zebra/zebra_rib.c
@@ -375,8 +375,7 @@ nexthop_active_ipv4 (struct rib *rib, struct nexthop *nexthop, int set,
 	      
 	      return 1;
 	    }
-	  else if (CHECK_FLAG (rib->flags, ZEBRA_FLAG_INTERNAL) ||
-		   match->type == ZEBRA_ROUTE_KERNEL)
+	  else if (CHECK_FLAG (rib->flags, ZEBRA_FLAG_INTERNAL))
 	    {
 	      for (newhop = match->nexthop; newhop; newhop = newhop->next)
 		if (CHECK_FLAG (newhop->flags, NEXTHOP_FLAG_FIB)
